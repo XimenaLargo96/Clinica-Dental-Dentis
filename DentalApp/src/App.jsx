829d6c5a8eb5d324eb0e44c9a9f9b2a5e07c087c
XimenaLargo96 <ximena0301@gmail.com>
2023-04-26 21:19:36 -0500

    Cambios en el navbar

--- DentalApp/src/App.jsx
+++ DentalApp/src/App.jsx
@@ -1,10 +1,12 @@
 import Card from './Components/Card'
+import Navbar from './Components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Navbar />
       <Card />
       <Card />
       <Card />
--- DentalApp/src/Components/Navbar.jsx
+++ DentalApp/src/Components/Navbar.jsx
@@ -2,6 +2,7 @@
 export default function Navbar() {
   return (
     <nav>
+      <img src="../images/DH.png" width= "200px"/>
       <ul>
         <li>Pacientes</li>
         <li>Odontologos</li>
