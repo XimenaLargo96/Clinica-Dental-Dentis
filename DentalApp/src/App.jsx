297b03e36ba5d19894cb2be4c1ee2d95cd257bc0
XimenaLargo96 <ximena0301@gmail.com>
2023-05-01 23:24:39 -0500

    Agrego redireccion al home

--- DentalApp/src/App.jsx
+++ DentalApp/src/App.jsx
@@ -2,8 +2,17 @@
 import Navbar from './Components/Navbar'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import { useLocation , useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+
+  const location = useLocation();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		location.pathname === "/" && navigate("/home");
+	}, []);
 
   return (
     <>
