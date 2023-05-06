bf89449ac34f0a4608b2b155d8caec582f4ddbbd
XimenaLargo96 <ximena0301@gmail.com>
2023-05-05 19:57:09 -0500

    Agrego validacion de inputs

--- DentalApp/src/routes/Shieft.jsx
+++ DentalApp/src/routes/Shieft.jsx
@@ -1,13 +1,27 @@
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Shieft() {
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <>
       <h1 className="m-3">Turnos Clinica Dental</h1>
       <Container className="mt-5">
         <Row className="justify-content-md-center">
           <Card style={{ width: "auto" }}>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <Row className="mb-3">
                 <Form.Group
                   className="mb-3"
@@ -16,7 +30,10 @@ export default function Shieft() {
                   controlId="formBasicEmail"
                 >
                   <Form.Label>Id del paciente </Form.Label>
-                  <Form.Control type="text" placeholder="paciente" />
+                  <Form.Control type="text" placeholder="paciente" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide the patient id.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group
@@ -26,7 +43,10 @@ export default function Shieft() {
                   controlId="formBasicPassword"
                 >
                   <Form.Label>Id del odontologo</Form.Label>
-                  <Form.Control type="text" placeholder="odontologo" />
+                  <Form.Control type="text" placeholder="odontologo" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide the dentist id.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group
@@ -36,7 +56,10 @@ export default function Shieft() {
                   controlId="formBasicPassword"
                 >
                   <Form.Label>Fecha del turno</Form.Label>
-                  <Form.Control type="date" style={{ cursor: "pointer" }} />
+                  <Form.Control type="date" style={{ cursor: "pointer" }} required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a date.
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Row>
               <Button variant="primary" type="submit">
