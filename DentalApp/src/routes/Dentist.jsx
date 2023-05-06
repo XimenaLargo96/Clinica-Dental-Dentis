b6c5ee649a618fc65ed487ce160af1c15523a649
XimenaLargo96 <ximena0301@gmail.com>
2023-05-05 19:41:10 -0500

    Cambio de estilos del form

--- DentalApp/src/routes/Dentist.jsx
+++ DentalApp/src/routes/Dentist.jsx
@@ -1,35 +1,52 @@
-import { Button, Row, Container, Form, Card } from "react-bootstrap";
+import { Button, Row, Col, Container, Form, Card } from "react-bootstrap";
 
 export default function Dentist() {
   return (
     <>
-    <h1 className="m-3">Odontologos Clinica Dental</h1>
-    <Container className="mt-5">
-      <Row className="justify-content-md-center">
-        <Card style={{ width: "18rem" }}>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Nombre </Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
+      <h1 className="m-3">Odontologos Clinica Dental</h1>
+      <Container className="mt-5">
+        <Row className="justify-content-md-center">
+          <Card style={{ width: "auto" }}>
+            <Form>
+              <Row className="mb-3">
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Nombre </Form.Label>
+                  <Form.Control type="text" placeholder="Enter name" />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Apellido</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last name" />
-            </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Apellido</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Last name" />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Matricula</Form.Label>
-              <Form.Control type="text" placeholder="Enter nit" />
-            </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Matricula</Form.Label>
+                  <Form.Control type="text" placeholder="Enter nit" />
+                </Form.Group>
+              </Row>
 
-            <Button variant="primary" type="submit">
-              Registrar
-            </Button>
-          </Form>
-        </Card>
-      </Row>
-    </Container>
+              <Button variant="primary" type="submit">
+                Registrar
+              </Button>
+            </Form>
+          </Card>
+        </Row>
+      </Container>
     </>
   );
 }
--- DentalApp/src/routes/Shieft.jsx
+++ DentalApp/src/routes/Shieft.jsx
@@ -1,28 +1,44 @@
-import { Container , Row , Card , Form , Button } from "react-bootstrap"
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
 export default function Shieft() {
   return (
     <>
-    <h1 className="m-3">Turnos Clinica Dental</h1>
-    <Container className="mt-5">
+      <h1 className="m-3">Turnos Clinica Dental</h1>
+      <Container className="mt-5">
         <Row className="justify-content-md-center">
-          <Card style={{ width: "auto"}}>
+          <Card style={{ width: "auto" }}>
             <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Id del paciente </Form.Label>
-                <Form.Control type="text" placeholder="paciente" />
-              </Form.Group>
+              <Row className="mb-3">
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Id del paciente </Form.Label>
+                  <Form.Control type="text" placeholder="paciente" />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Id del odontologo</Form.Label>
-                <Form.Control type="text" placeholder="odontologo" />
-              </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Id del odontologo</Form.Label>
+                  <Form.Control type="text" placeholder="odontologo" />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Fecha del turno</Form.Label>
-                <Form.Control type="date" style={{ cursor: "pointer" }} />
-              </Form.Group>
-
+                <Form.Group
+                  className="mb-3"
+                  as={Col}
+                  md="4"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Fecha del turno</Form.Label>
+                  <Form.Control type="date" style={{ cursor: "pointer" }} />
+                </Form.Group>
+              </Row>
               <Button variant="primary" type="submit">
                 Registrar
               </Button>
@@ -31,5 +47,5 @@ export default function Shieft() {
         </Row>
       </Container>
     </>
-  )
+  );
 }
