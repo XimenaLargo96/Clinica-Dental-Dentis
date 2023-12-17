f19b7b8e11c3e2bdcd331c76592f3263de1b6fc4
Ximena Largo <ximena0301@gmail.com>
2023-12-16 20:41:32 -0500

    Add new component and restructuring home component

--- DentalApp/src/Components/Home.jsx
+++ DentalApp/src/Components/Home.jsx
@@ -1,43 +1,12 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Card from "./Card";
+import React from 'react';
 import CarouselImg from "./CarouselImg.jsx";
+import Servicies from "./Servicies.jsx";
 
 export default function Home() {
-  const cardValues = [
-    {
-      title: "Odontologos",
-      description: "Registra nuevos odontologos en tu clínica dental.",
-      image: "../images/odontologo.jpg",
-    },
-    {
-      title: "Pacientes",
-      description: "Registra a tus nuevos pacientes, rápido y fácil.",
-      image: "../images/paciente.jpg",
-    },
-    {
-      title: "Turnos",
-      description: "Registra nuevos turnos en tu clínica dental.",
-      image: "../images/turno.png",
-    },
-  ];
-
   return (
     <>
       <CarouselImg />
-      <Container className="mt-5">
-        <Row xs={1} md={3} className="g-4">
-          {cardValues.map((item, index) => (
-            <Col key={index}>
-              <Card
-                key={item.title}
-                title={item.title}
-                img={item.image}
-                description={item.description}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Servicies />
     </>
   );
 }
--- DentalApp/src/Components/Servicies.jsx
+++ DentalApp/src/Components/Servicies.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Card from "./Card";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Servicies() {
+
+  const cardValues = [
+    {
+      title: "Odontologos",
+      description: "Registra nuevos odontologos en tu clínica dental.",
+      image: "../images/odontologo.jpg",
+    },
+    {
+      title: "Pacientes",
+      description: "Registra a tus nuevos pacientes, rápido y fácil.",
+      image: "../images/paciente.jpg",
+    },
+    {
+      title: "Turnos",
+      description: "Registra nuevos turnos en tu clínica dental.",
+      image: "../images/turno.png",
+    },
+  ];
+
   return (
-    <div>Servicies</div>
+    <>
+    <h2 className='text-center fs-1 pt-5'>Nuestros Servicios</h2>
+    <Container className="mt-5 mb-5">
+        <Row xs={1} md={3} className="g-4">
+          {cardValues.map((item, index) => (
+            <Col key={index}>
+              <Card
+                key={item.title}
+                title={item.title}
+                img={item.image}
+                description={item.description}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   )
 }
