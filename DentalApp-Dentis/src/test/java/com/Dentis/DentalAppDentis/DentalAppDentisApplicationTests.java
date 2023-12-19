72b8d9897d86a30f3cc9eb5241910d980e627c71
Ximena Largo <ximena0301@gmail.com>
2023-12-19 11:28:47 -0500

    creation of the backend structure

---
+++ DentalApp-Dentis/src/test/java/com/Dentis/DentalAppDentis/DentalAppDentisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dentis.DentalAppDentis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalAppDentisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
