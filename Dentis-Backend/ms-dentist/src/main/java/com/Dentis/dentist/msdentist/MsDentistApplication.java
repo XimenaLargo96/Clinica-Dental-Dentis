8c2c5c3cd9d58207cabd46097001551299aea5a3
XimenaLargo96 <ximena0301@gmail.com>
2024-04-08 17:59:55 -0500

    Add ms-dentist to the project

---
+++ Dentis-Backend/ms-dentist/src/main/java/com/Dentis/dentist/msdentist/MsDentistApplication.java
@@ -0,0 +1,13 @@
+package com.Dentis.dentist.msdentist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDentistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDentistApplication.class, args);
+	}
+
+}
---
+++ Dentis-Backend/ms-dentist/src/test/java/com/Dentis/dentist/msdentist/MsDentistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dentis.dentist.msdentist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDentistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
