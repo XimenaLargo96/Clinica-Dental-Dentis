bf7ea3ef8177ddae9b4f89237a52d959180ba4a2
XimenaLargo96 <ximena0301@gmail.com>
2024-04-11 14:27:40 -0500

    Add ms-appointment to the project

---
+++ Dentis-Backend/ms-appointment/src/test/java/com/Dentis/appointment/msappointment/MsAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dentis.appointment.msappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
